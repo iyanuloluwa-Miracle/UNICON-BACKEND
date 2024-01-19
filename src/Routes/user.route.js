3456fec29f77643ca6010ab9b12a939e37dddc0e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-19 16:56:51 +0100

    Fix: Bug fix

--- src/Routes/user.route.js
+++ src/Routes/user.route.js
@@ -2,21 +2,20 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../Controllers/userController');
 const { verifyToken } = require("../Utils/authUtils");
-const { validateEvent } = require("../Validators/users");
 
 // Route to get all users
-router.get('/users', verifyToken, validateEvent, UserController.getAllUsers);
+router.get('/users', verifyToken, UserController.getAllUsers);
 
 // Route to get a user by ID
-router.get('/users/:id', verifyToken, validateEvent, UserController.getUserById);
+router.get('/users/:id', verifyToken, UserController.getUserById);
 
 // Route to search for users
-router.get('/users/search', verifyToken, validateEvent, UserController.searchUsers);
+router.get('/users/search', verifyToken, UserController.searchUsers);
 
 // Route to update a user
-router.put('/users/:id', verifyToken, validateEvent, UserController.updateUser);
+router.put('/users/:id', verifyToken, UserController.updateUser);
 
 // Route to delete a user
-router.delete('/users/:id',verifyToken, validateEvent,UserController.deleteUser);
+router.delete('/users/:id',verifyToken,UserController.deleteUser);
 
 module.exports = router;
